e417c67ce21215908af5d4463d7eac0120b8796c
charlierooney922-maker <charlierooney922@gmail.com>
2026-06-07 21:32:55 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'lifedash-v1';
+const ASSETS = [
+  '/life-dashboard/',
+  '/life-dashboard/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
